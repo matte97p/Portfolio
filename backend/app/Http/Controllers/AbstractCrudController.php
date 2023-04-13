ecc39e13ac535d63f35619c03f235662a972e841
Matteo Perino <matteo.perino@gmail.com>
2023-04-13 15:55:47 +0200

    fix

--- backend/app/Http/Controllers/AbstractCrudController.php
+++ backend/app/Http/Controllers/AbstractCrudController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use GuzzleHttp\Utils;
-use GuzzleHttp\Client;
 use App\Utils\Logger\Logger;
 use Illuminate\Http\Request;
 use GuzzleHttp\RequestOptions;
--- backend/app/Http/Controllers/AbstractGenericController.php
+++ backend/app/Http/Controllers/AbstractGenericController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
 use App\Utils\Logger\Logger;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
